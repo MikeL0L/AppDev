0f8c10cbc595b135d914274c1cb85efb8f3d4b22
Michael Andres <144314027+MikeL0L@users.noreply.github.com>
2025-03-12 10:34:46 +0800

    Update view_faculty_hours_screen.dart

--- view_faculty_hours_screen.dart
+++ view_faculty_hours_screen.dart
@@ -6,15 +6,34 @@ class ViewFacultyHoursScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Faculty Office Hours'),
-        backgroundColor: Colors.cyan.shade600,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors:  [
+                Color(0xFFFF971A),
+                Color(0xFFFFFF67),
+              ],
+                  transform: GradientRotation(24)
+              )
+          ),
+        ),
       ),
-      body: ListView(
+        body: Container(
+        decoration: BoxDecoration(
+        image: DecorationImage(
+        image: AssetImage('assets/intro-background.png'),
+        fit: BoxFit.cover,
+            ),
+        ),
+      child: ListView(
         children: [
-          ListTile(title: Text('Dr. Smith: Mon-Fri, 10 AM - 12 PM', style: TextStyle(color: Colors.white))),
-          ListTile(title: Text('Prof. Johnson: Tue-Thu, 1 PM - 3 PM ', style: TextStyle(color: Colors.white))),
-          ListTile(title: Text('Ms. Lee: Wed, 2 PM - 4 PM', style: TextStyle(color: Colors.white))),
-        ],
-      ),
+          ListTile(title: Text('Dr. Smith: Mon-Fri, 10 AM - 12 PM', style: TextStyle(color: Colors.black))),
+          ListTile(title: Text('Prof. Johnson: Tue-Thu, 1 PM - 3 PM ', style: TextStyle(color: Colors.black))),
+          ListTile(title: Text('Ms. Lee: Wed, 2 PM - 4 PM', style: TextStyle(color: Colors.black))),
+            ],
+          ),
+        ),
     );
   }
 }
