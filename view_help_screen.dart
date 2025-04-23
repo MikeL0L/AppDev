12e1aea73133f4d0fa794861057c625090e2408b
Michael Andres <144314027+MikeL0L@users.noreply.github.com>
2025-04-23 10:24:21 +0800

    removed view alert instructions and others

--- view_help_screen.dart
+++ view_help_screen.dart
@@ -5,11 +5,9 @@ class ViewHelpScreen extends StatelessWidget {
     'Log in using your 10-digit Student ID as both username and password.',
     'Guests can use the app without logging in but will have limited features.',
     'Access the campus map and tap on buildings for more details.',
-    'Students can drag and drop class schedules to customize their timetable.',
-    'Receive alerts for upcoming classes and exams in the student dashboard.',
+    'Students can add class schedules to customize their timetable.',
     'Tap on emergency contacts from the menu for quick access to help.',
     'Check your profile from the profile screen to see your personal information.',
-    'Use the app’s search bar to locate classrooms, offices, and facilities.',
     'View faculty office hours directly from the class detail screen.'
   ];
 
